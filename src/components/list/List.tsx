fda07f53a16abe49a9d6b02b263025d9a15b54b8
mynameistaeyoung <cv4660@naver.com>
2024-08-14 13:47:15 +0900

    feat: 삭제 및 완료 기능 추가 #3

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,19 +1,42 @@
-import { ListContainer,ListWrapper } from "./StyledList"
+import { ListContainer, ListWrapper } from "./StyledList"
 import Todo from "../todo/Todo"
+import { useTodoStore } from "../../zustand/bearsStore"
 
 function List() {
-    return (
-        <ListContainer>
-          <h2>이거 해야 함..🔥</h2>
-          <ListWrapper>
-            <Todo/>
-          </ListWrapper>
-          <h2>다 했다..!🎉</h2>
-          <ListWrapper>
-          <Todo/>
-          </ListWrapper>
-        </ListContainer>
-      )
+  const { todos, deleteTodo, editTodo } = useTodoStore(state => state)
+
+  const onDeleteTodoHandler = (todoId) => {
+    deleteTodo(todoId)
+  }
+
+  const onEditTodoHandler = (todoId) => {
+    editTodo(todoId)
+  }
+  
+  return (
+    <ListContainer>
+      <h2>이거 해야 함..🔥</h2>
+      <ListWrapper>
+        {todos.map(item => !item.completed ?
+          <Todo
+            item={item}
+            key={item.id}
+            onDeleteTodoHandler={onDeleteTodoHandler}
+            onEditTodoHandler={onEditTodoHandler}
+          /> : null)}
+      </ListWrapper>
+      <h2>다 했다..!🎉</h2>
+      <ListWrapper>
+        {todos.map(item => item.completed ?
+          <Todo
+            item={item}
+            key={item.id}
+            onDeleteTodoHandler={onDeleteTodoHandler}
+            onEditTodoHandler={onEditTodoHandler}
+          /> : null)}
+      </ListWrapper>
+    </ListContainer>
+  )
 }
 
 export default List
