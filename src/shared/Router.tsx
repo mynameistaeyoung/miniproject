4ad306c83395eca2a013516fd6d86b659ff51754
mynameistaeyoung <cv4660@naver.com>
2024-08-15 12:45:43 +0900

    feat: 디테일 페이지 라우턴 추가

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -4,12 +4,14 @@ import Home from '../pages/home/Home';
 import Login from '../pages/accountmanagement/login/Login';
 import Register from '../pages/accountmanagement/register/Register';
 import FindPassword from '../pages/accountmanagement/findpassword/FindPassword';
+import Detail from '../pages/detail/Detail';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 < Route path="/" element={< Home />} />
+                < Route path="/:id" element={< Detail />} />
                 < Route path="/login" element={< Login />} />
                 < Route path="/register" element={< Register />} />
                 < Route path="/findPassword" element={< FindPassword />} />
