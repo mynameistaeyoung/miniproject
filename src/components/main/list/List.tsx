de842698a72e7fb9c349fdbd7e51a54a164ce7c3
mynameistaeyoung <cv4660@naver.com>
2024-08-17 21:45:09 +0900

    component: 리스트 컴포넌트 제작

---
+++ src/components/main/list/List.tsx
@@ -0,0 +1,46 @@
+import { ListContainer, ListWrapper } from "./StyledList"
+import Todo from "../todo/Todo"
+import { useTodoStore } from "../../../zustand/bearsStore"
+
+
+
+
+
+function List() {
+  const { todos, deleteTodo, editTodo } = useTodoStore(state => state)
+
+  const onDeleteTodoHandler = (todoId:number) => {
+    deleteTodo(todoId)
+  }
+
+  const onEditTodoHandler = (todoId:number) => {
+    editTodo(todoId)
+  }
+  
+  return (
+    <ListContainer>
+      <h2>이거 해야 함..🔥</h2>
+      <ListWrapper>
+        {todos.map(item => !item.completed ?
+          <Todo
+            item={item}
+            key={item.id}
+            onDeleteTodoHandler={onDeleteTodoHandler}
+            onEditTodoHandler={onEditTodoHandler}
+          /> : null)}
+      </ListWrapper>
+      <h2>다 했다..!🎉</h2>
+      <ListWrapper>
+        {todos.map(item => item.completed ?
+          <Todo
+            item={item}
+            key={item.id}
+            onDeleteTodoHandler={onDeleteTodoHandler}
+            onEditTodoHandler={onEditTodoHandler}
+          /> : null)}
+      </ListWrapper>
+    </ListContainer>
+  )
+}
+
+export default List
