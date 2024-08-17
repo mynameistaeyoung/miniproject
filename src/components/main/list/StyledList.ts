b4150813e3f56545b6a58e6c072b96f540e39f90
mynameistaeyoung <cv4660@naver.com>
2024-08-17 21:45:30 +0900

    style: 리스트 CSS 작업

---
+++ src/components/main/list/StyledList.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const ListContainer=styled.div`
+    padding: 0 24px;
+`
+const ListWrapper=styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 12px;
+`
+
+export {ListContainer,ListWrapper}
