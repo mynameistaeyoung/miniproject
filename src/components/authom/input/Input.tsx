005aad6721dbac6558f3179613c9f6db64819a2a
mynameistaeyoung <cv4660@naver.com>
2024-08-17 21:38:42 +0900

    refactor: 인풋을 재사용하기 좋게 변경함

--- src/components/authom/input/Input.tsx
+++ src/components/authom/input/Input.tsx
@@ -1,35 +1,73 @@
 import styled from "styled-components"
 
 type UserProps = {
-  htmlFor: string;
-  id: string;
-  type: string;
-  placeholder: string;
-  children: string;
+    htmlFor: string;
+    id: string;
+    type: string;
+    placeholder: string;
+    children: string;
+    size: string;
+    value?: string;
+    onChange?: (e: any) => void
 }
-function Input({ htmlFor, id, type, placeholder, children }: UserProps) {
-  return (
-    <StInputContainer>
-      <label htmlFor={htmlFor}>{children}</label>
-      <StInput
-        id={id}
-        type={type}
-        placeholder={placeholder}
-      />
-    </StInputContainer>
-  )
+
+interface StInputProps {
+    border: string;
+    borderRadius: string;
+    width: string;
+    padding: string;
+    marginTop?: string;
+    marginBottom?: string;
+    height: string;
+}
+
+
+function Input({ htmlFor, id, type, placeholder, children, size, value, onChange }: UserProps) {
+    return (
+        size === "Medium" ?
+            <StInputContainer>
+                <label htmlFor={htmlFor}>{children}</label>
+                <StInput
+                    border="1px solid gray"
+                    borderRadius="8px"
+                    width="95%"
+                    height=''
+                    padding='10px'
+                    marginTop='5px'
+                    marginBottom="15px"
+                    id={id}
+                    type={type}
+                    placeholder={placeholder}
+                />
+            </StInputContainer> :
+            <div>
+                <StFormLabel htmlFor={htmlFor}>{children}</StFormLabel>
+                <StInput
+                    height="40px"
+                    width="600px"
+                    border="none"
+                    borderRadius="12px"
+                    padding="0 12px"
+                    value={value}
+                    onChange={onChange}
+                    id={id}
+                    type={type}
+                    placeholder={placeholder}
+                />
+            </div>
+    )
 }
 
 export default Input
 
-
-const StInput = styled.input`
-  border:1px solid gray;
-  border-radius:8px;
-  width: 95%;
-  padding: 10px;
-  margin-top: 5px;
-  margin-bottom: 15px;
+const StInput = styled.input<StInputProps>`
+    border:${({ border }) => border};
+    border-radius:${({ borderRadius }) => borderRadius};
+    height:${({ height }) => height};
+    width:${({ width }) => width};
+    padding:${({ padding }) => padding};
+    margin-top:${({ marginTop }) => marginTop};
+    margin-bottom:${({ marginBottom }) => marginBottom};
 `
 
 const StInputContainer = styled.div`
@@ -37,4 +75,11 @@ const StInputContainer = styled.div`
   flex-direction:column;
 `
 
-export { StInput }
+const StFormLabel = styled.label`
+  font-size: 16px;
+  font-weight: 700;
+  margin-right:30px;
+`;
+
+
+
