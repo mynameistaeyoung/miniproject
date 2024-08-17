59b9d60912c80342c598cb35528dbd901392135f
mynameistaeyoung <cv4660@naver.com>
2024-08-17 21:42:15 +0900

    refactor: 인풋과 버튼 컴포넌트에서 존재하기 때문에 불필요한 컴포넌트 삭제

---
+++ src/components/main/form/StyledForm.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StAddForm = styled.form`
+  background-color: #eee;
+  border-radius: 12px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 30px;
+  gap: 20px;
+`;
+
+export {StAddForm}
